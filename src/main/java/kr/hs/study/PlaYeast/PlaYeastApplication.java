f84755ef59fea1c60743ad23ad80c929ef49b6ca
yes25 <programmer.yes25@gmail.com>
2023-07-25 15:42:45 +0900

    Initial commit

---
+++ src/main/java/kr/hs/study/PlaYeast/PlaYeastApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.PlaYeast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaYeastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaYeastApplication.class, args);
+	}
+
+}
