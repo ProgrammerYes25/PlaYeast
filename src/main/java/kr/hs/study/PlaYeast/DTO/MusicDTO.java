836811764235fb33913111655a4956fd471dbd1d
yes25 <programmer.yes25@gmail.com>
2023-08-03 15:49:56 +0900

    입력 페이지 작업

---
+++ src/main/java/kr/hs/study/PlaYeast/DTO/MusicDTO.java
@@ -0,0 +1,13 @@
+package kr.hs.study.PlaYeast.DTO;
+
+import lombok.Data;
+
+@Data
+public class MusicDTO {
+    int music_id;
+    String music_name;
+    String music_artist;
+    String music_album;
+    int music_time;
+    int music_track;
+}
