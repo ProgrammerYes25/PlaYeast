8b49026f623f8c9f309048e570518a7edefc108e
yes25 <programmer.yes25@gmail.com>
2023-08-21 14:54:52 +0900

    auto wired 한 넣은 후부터 오류 발생 고치는 중

--- src/main/java/kr/hs/study/PlaYeast/Controllers/PalYeastController.java
+++ src/main/java/kr/hs/study/PlaYeast/Controllers/PalYeastController.java
@@ -2,12 +2,14 @@ package kr.hs.study.PlaYeast.Controllers;
 
 import kr.hs.study.PlaYeast.DTO.MusicDTO;
 import kr.hs.study.PlaYeast.Service.PlaYeastService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PalYeastController {
+    @Autowired
     private PlaYeastService plaYeastService;
     @GetMapping("/")
     public String index(){
--- src/main/java/kr/hs/study/PlaYeast/Service/PlaYeastServiceImpl.java
+++ src/main/java/kr/hs/study/PlaYeast/Service/PlaYeastServiceImpl.java
@@ -2,10 +2,12 @@ package kr.hs.study.PlaYeast.Service;
 
 import kr.hs.study.PlaYeast.DTO.MusicDTO;
 import kr.hs.study.PlaYeast.Mappers.PlaYeastMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlaYeastServiceImpl implements PlaYeastService{
+    @Autowired
     public PlaYeastMapper plaYeastMapper;
     @Override
     public void muiscInsert(MusicDTO dto) {
