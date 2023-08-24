1adeda50a7d5624d33d011795f28e5228148326f
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-08-25 03:42:25 +0900

    css 수정

--- src/main/java/kr/hs/study/PlaYeast/DTO/PlayListDTO.java
+++ src/main/java/kr/hs/study/PlaYeast/DTO/PlayListDTO.java
@@ -1,4 +1,15 @@
 package kr.hs.study.PlaYeast.DTO;
 
 public class PlayListDTO {
+    String music_name;
+    String music_artist;
+    String music_album;
+    int music_time;
+    int music_track;
+    String genre_id;
+    String mood_id;
+    String weather_id;
+    String genre_name;
+    String mood_name;
+    String weather_name;
 }
